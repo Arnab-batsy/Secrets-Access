eab100088fb3330ca9bd08537f48b8c878baa1a3
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2024-01-20 01:25:49 +0530

    Add functionality

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app= express();
+const port= 3000;
+var isCorrect= false;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function check(req,res,next){
+    if(req.body["password"]==="ILoveProgramming"){
+        isCorrect=true;
+    }
+    next();
+}
+
+app.use(check);
+
+app.post("/check",(req,res)=>{
+    if(isCorrect==true){
+        res.sendFile(__dirname+"/public/secret.html")
+    }
+    else{
+        res.sendFile(__dirname + "/public/index.html");
+    }
+})
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port,()=>{
+    console.log(`Server running at port ${port}`);
+});
